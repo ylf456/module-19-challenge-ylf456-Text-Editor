be115869ab8b0f667f1fe1a3067bd0512d02622b
ylf456 <145236890+ylf456@users.noreply.github.com>
2024-02-27 16:25:32 -0800

    fixed logic in install.js where the button will not be hidden after installation

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -5,32 +5,49 @@ const butInstall = document.getElementById("buttonInstall");
 window.addEventListener("beforeinstallprompt", (event) => {
   console.log('before install')
   // Store the triggered events
+  if(!event){
+    console.log("This app is already installed");
+    butInstall.classList.toggle("hidden", true);
+    return;
+  }
   window.deferredPrompt = event;
   // Remove the hidden class from the button.
   butInstall.classList.toggle("hidden", false);
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener("click", async () => {
-  const promptEvent = window.deferredPrompt; // retrieves the stored prompt event from window.deferredPrompt.
-  // If there is a prompt event available,
-  // it calls the prompt() method on it to show the installation prompt to the user.
-  if (!promptEvent) {
-    // if there is no promptEvent then return
-    return;
-  }
-  // Show prompt
-  promptEvent.prompt(); // prompt user , and if true, start installation process
-  // Reset the deferred prompt variable, it can only be used once.
-  // (clear the install event from window.deferredPrompt variable)
-  console.log("successfully installed")
-  window.deferredPrompt = null;
+if (!window.deferredPrompt) {
+  console.log("This App was already Installed");
   butInstall.classList.toggle("hidden", true);
-});
+} else {
+  
+  butInstall.addEventListener("click", async () => {
+    const promptEvent = window.deferredPrompt; // retrieves the stored prompt event from window.deferredPrompt.
+    // If there is a prompt event available,
+    // it calls the prompt() method on it to show the installation prompt to the user.
+    if (!promptEvent) {
+      // if there is no promptEvent then return
+      console.log("No event is added to this button");
+      butInstall.classList.toggle("hidden", true);
+      return;
+    }
+    // Show prompt
+    promptEvent.prompt(); // prompt user , and if true, start installation process
+    // Reset the deferred prompt variable, it can only be used once.
+    // (clear the install event from window.deferredPrompt variable)
+    console.log("successfully installed");
+    window.deferredPrompt = null;
+    butInstall.classList.toggle("hidden", true);
+
+  });
+  
+}
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener("appinstalled", (event) => {
-  console.log("already installed")
+  console.log("already installed");
+  butInstall.classList.toggle("hidden", true);
   // Clear prompt (clear the install event from window.deferredPrompt variable)
   window.deferredPrompt = null;
+  
 });
